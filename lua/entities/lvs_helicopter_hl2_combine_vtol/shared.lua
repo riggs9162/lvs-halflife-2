bdc7dd98c085c9da56ac7c9dbe21cc241386cf6c
Riggs <riggs9162@gmx.de>
2026-03-21 03:02:55 +0100

    refactor(helicopter): improve code formatting and readability

--- lua/entities/lvs_helicopter_hl2_combine_vtol/shared.lua
+++ lua/entities/lvs_helicopter_hl2_combine_vtol/shared.lua
@@ -1,50 +1,31 @@
 ENT.Base = "lvs_base_helicopter"
-
 ENT.PrintName = "Combine VTOL"
-ENT.Author = "Luna"
-ENT.Information = "Combine VTOL, used to transport troops and supplies."
+ENT.Author = "Riggs"
+ENT.Information = ""
 ENT.Category = "[LVS] - Half Life 2"
 ENT.IconOverride = "materials/entities/combinevtol.png"
 
-ENT.VehicleCategory = "Half Life 2"
-ENT.VehicleSubCategory = "Combine"
-
-ENT.Spawnable			= util.IsValidModel("models/ludex/mirrors_edge_catalyst/vtol_attack_ragdoll.mdl")
-ENT.AdminSpawnable		= false
-
-ENT.DisableBallistics = true
-
-ENT.MDL = "models/ludex/mirrors_edge_catalyst/vtol_attack_ragdoll.mdl"
-ENT.GibModels = {
-	"models/gibs/helicopter_brokenpiece_01.mdl",
-	"models/gibs/helicopter_brokenpiece_02.mdl",
-	"models/gibs/helicopter_brokenpiece_03.mdl",
-	"models/gibs/helicopter_brokenpiece_06_body.mdl",
-	"models/gibs/helicopter_brokenpiece_04_cockpit.mdl",
-	"models/gibs/helicopter_brokenpiece_05_tailfan.mdl",
-}
-
 ENT.AITEAM = 1
-
+ENT.AdminSpawnable = false
+ENT.DisableBallistics = true
+ENT.ForceAngleDampingMultiplier = 1
+ENT.ForceAngleMultiplier = 1
+ENT.ForceLinearDampingMultiplier = 1.5
+ENT.GibModels = {"models/gibs/helicopter_brokenpiece_01.mdl", "models/gibs/helicopter_brokenpiece_02.mdl", "models/gibs/helicopter_brokenpiece_03.mdl", "models/gibs/helicopter_brokenpiece_06_body.mdl", "models/gibs/helicopter_brokenpiece_04_cockpit.mdl", "models/gibs/helicopter_brokenpiece_05_tailfan.mdl"}
+ENT.MDL = "models/ludex/mirrors_edge_catalyst/vtol_attack_ragdoll.mdl"
 ENT.MaxHealth = 1400
-
 ENT.MaxVelocity = 1500
-
-ENT.ThrustUp = 0.5
+ENT.Spawnable = file.Exists("models/ludex/mirrors_edge_catalyst/vtol_attack_ragdoll.mdl", "GAME") 
+ENT.ThrottleRateDown = 0.1
+ENT.ThrottleRateUp = 0.1
 ENT.ThrustDown = 0.5
 ENT.ThrustRate = 0.5
-
-ENT.ThrottleRateUp = 0.1
-ENT.ThrottleRateDown = 0.1
-
+ENT.ThrustUp = 0.5
 ENT.TurnRatePitch = 0.25
-ENT.TurnRateYaw = 0.5
 ENT.TurnRateRoll = 0.5
-
-ENT.ForceLinearDampingMultiplier = 1.5
-
-ENT.ForceAngleMultiplier = 1
-ENT.ForceAngleDampingMultiplier = 1
+ENT.TurnRateYaw = 0.5
+ENT.VehicleCategory = "Half Life 2"
+ENT.VehicleSubCategory = "Combine"
 
 ENT.EngineSounds = {
 	{
