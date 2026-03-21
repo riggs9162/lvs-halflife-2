d3bbdd3170175bfdf73028e3282e7dc50bd52dcb
Riggs <riggs9162@gmx.de>
2026-03-21 03:00:46 +0100

    refactor(combine_hunter_bomb): streamline entity code and improve sound handling
    Removes needless formatting and optimizes timer and physics logic for the Combine hunter bomb.

--- lua/entities/lvs_helicopter_hl2_combine_hunter_bomb.lua
+++ lua/entities/lvs_helicopter_hl2_combine_hunter_bomb.lua
@@ -1,22 +1,18 @@
 AddCSLuaFile()
-
 ENT.Base = "lvs_bomb"
-DEFINE_BASECLASS( "lvs_bomb" )
-
-ENT.Spawnable       = false
-ENT.AdminSpawnable  = false
+DEFINE_BASECLASS("lvs_bomb")
+ENT.Spawnable = false
+ENT.AdminSpawnable = false
 ENT.DoNotDuplicate = true
-
 ENT.ExplosionEffect = "lvs_explosion_small"
-
 function ENT:Initialize()
 	self:SetModel("models/Combine_Helicopter/helicopter_bomb01.mdl")
-	self:SetMoveType( MOVETYPE_NONE )
-	self:SetSkin( 1 )
+	self:SetMoveType(MOVETYPE_NONE)
+	self:SetSkin(1)
 end
 
 if SERVER then
-	function ENT:OnTakeDamage( dmginfo )
+	function ENT:OnTakeDamage(dmginfo)
 		if self.IsTimerStarted then
 			self:Detonate()
 		else
@@ -24,15 +20,11 @@ if SERVER then
 		end
 	end
 
-	function ENT:Think()	
+	function ENT:Think()
 		local T = CurTime()
-
-		self:NextThink( T )
-
+		self:NextThink(T)
 		self:UpdateTrajectory()
-
 		if not self.SpawnTime then return true end
-
 		if (self.SpawnTime + 30) < T then
 			if self.IsTimerStarted then
 				self:Detonate()
@@ -40,52 +32,41 @@ if SERVER then
 				self:Destroy()
 			end
 		end
-
 		return true
 	end
 
 	function ENT:StartDetonationTimer()
 		if self.IsTimerStarted then return end
-
 		self.IsTimerStarted = true
-
-		self.snd = CreateSound( self, "npc/attack_helicopter/aheli_mine_seek_loop1.wav" )
-		self.snd:PlayEx( 0, 100 )
-		self.snd:ChangeVolume(1, 0.5 )
-
-		self:SetSkin( 0 )
-
+		self.snd = CreateSound(self, "npc/attack_helicopter/aheli_mine_seek_loop1.wav")
+		self.snd:PlayEx(0, 100)
+		self.snd:ChangeVolume(1, 0.5)
+		self:SetSkin(0)
 		timer.Simple(3, function()
-			if not IsValid( self ) or not self.snd then return end
-
-			self.snd:ChangePitch(160, 1 )
-
-			self:SetSkin( 1 )
-		end )
+			if not IsValid(self) or not self.snd then return end
+			self.snd:ChangePitch(160, 1)
+			self:SetSkin(1)
+		end)
 
 		timer.Simple(3.5, function()
-			if not IsValid( self ) then return end
-
-			self:SetSkin( 0 )
-		end )
+			if not IsValid(self) then return end
+			self:SetSkin(0)
+		end)
 
 		timer.Simple(3.75, function()
-			if not IsValid( self ) then return end
-
-			self:SetSkin( 1 )
-		end )
+			if not IsValid(self) then return end
+			self:SetSkin(1)
+		end)
 
 		timer.Simple(4, function()
-			if not IsValid( self ) then return end
-
+			if not IsValid(self) then return end
 			self:Detonate()
-		end )
+		end)
 	end
 
-	function ENT:PhysicsCollide( data )
-		if istable( self._FilterEnts ) and self._FilterEnts[ data.HitEntity ] then return end
-
-		if IsValid( data.HitEntity ) then
+	function ENT:PhysicsCollide(data)
+		if istable(self._FilterEnts) and self._FilterEnts[data.HitEntity] then return end
+		if IsValid(data.HitEntity) then
 			self:Detonate()
 		else
 			self:StartDetonationTimer()
@@ -94,16 +75,13 @@ if SERVER then
 
 	function ENT:Destroy()
 		if self.IsDestroyed then return end
-
 		self.IsDestroyed = true
-
-		local mdl = ents.Create( "prop_physics" )
-		mdl:SetModel( self:GetModel() )
-		mdl:SetPos( self:GetPos() )
-		mdl:SetAngles( self:GetAngles() )
+		local mdl = ents.Create("prop_physics")
+		mdl:SetModel(self:GetModel())
+		mdl:SetPos(self:GetPos())
+		mdl:SetAngles(self:GetAngles())
 		mdl:Spawn()
 		mdl:Fire("break")
-
 		self:Remove()
 	end
 
@@ -113,15 +91,12 @@ if SERVER then
 			self.snd = nil
 		end
 
-		BaseClass.OnRemove( self )
+		BaseClass.OnRemove(self)
 	end
-
 	return
 end
 
 function ENT:Enable()
 	if self.IsEnabled then return end
-
 	self.IsEnabled = true
 end
-
